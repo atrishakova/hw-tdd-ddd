abe33e8ce57d04702063ddc4e00f8fbcb3dbea36
Alexandra Trishakova <svmail97@gmail.com>
2026-05-04 22:56:03 +0500

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    void setUp() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    void addShouldReturnOneWhenFirstContactAdded() {
+        int size = phoneBook.add("Petya", "123456");
+        assertEquals(1, size);
+    }
+
+    @Test
+    void addShouldReturnTwoWhenTwoDifferentContactsAdded() {
+        phoneBook.add("Masha", "123456");
+        int size = phoneBook.add("Kolya", "789012");
+        assertEquals(2, size);
+    }
+
+    @Test
+    void addShouldNotAddDuplicateName() {
+        phoneBook.add("Petya", "123456");
+        int size = phoneBook.add("Petya", "999999");
+        assertEquals(1, size);
+    }
 }
